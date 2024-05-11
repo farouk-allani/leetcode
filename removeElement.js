211e76ca22eca2e72e932a12d937c7ba3da7418d
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-05-11 22:48:46 +0100

    remove element

---
+++ removeElement.js
@@ -0,0 +1,13 @@
+
+const nums =[0,1,2,2,3,0,4,2]
+const val = 2
+
+var removeElement = function(nums, val) {
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if(nums[i] == val){
+            nums.splice(i, 1);
+        }
+    }  
+};
+
