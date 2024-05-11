ec14ec7e737c5efa5f60e49495b4d8751ce9c7f6
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-05-11 23:23:51 +0100

    removeDuplicatesFromSortedArray

---
+++ removeDuplicatesFromSortedArray.js
@@ -0,0 +1,15 @@
+
+
+nums =
+[0,0,1,1,1,2,2,3,3,4]
+
+var removeDuplicates = function(nums) {
+    for(let i=nums.length-1; i>=0; i--) {
+        if(nums[i]==nums[i-1]){
+            nums.splice(i-1,1)
+        }
+    }
+    console.log(nums)
+};
+
+removeDuplicates(nums)
