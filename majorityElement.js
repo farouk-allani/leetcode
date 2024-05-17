5f8c91e37cab2c9c78fb39ff45b362b668e608fe
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-05-17 12:16:25 +0100

    majorityElement

--- majorityElement.js
+++ majorityElement.js
@@ -1,2 +1,22 @@
 
+const nums=[2,2,1,1,1,2,2]
+var majorityElement = function(nums) {
+    let hash={}
+    let size=nums.length
+    let majority=0
+    for(let n of nums) {
+        hash[n]=1+ (hash[n] || 0)
+        if(hash[n] > size/2){
+            majority=n
+        }
 
+
+    }
+
+    return majority
+
+
+    
+};
+
+majorityElement(nums)
