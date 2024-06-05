d20f6cce92f0f02ec413f3fda8e25309b8f487a3
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-06-05 09:38:23 +0100

    jumpGame

---
+++ jumpGame.js
@@ -0,0 +1,10 @@
+var canJump = function(nums) {
+    let maxJump=nums[0]
+    for (let i=1; i < nums.length; i++) {
+     if (i> maxJump) return false
+     maxJump= Math.max(maxJump, nums[i]+i)
+    }
+ 
+    return true
+ 
+ };
